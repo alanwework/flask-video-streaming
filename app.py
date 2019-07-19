945d393f281eaddddebd5ac3459592be36fbcde3
Alan Xing <alan.xing@wework.com>
2019-07-18 18:40:21 -0700

    Add opencv video streaming

--- app.py
+++ app.py
@@ -8,9 +8,14 @@ if os.environ.get('CAMERA'):
     Camera = import_module('camera_' + os.environ['CAMERA']).Camera
 else:
     from camera import Camera
+from camera_opencv import Camera as OpenCVCamera
+
 
 # Raspberry Pi camera module (requires picamera package)
 # from camera_pi import Camera
+
+ROOT_DIR = os.path.realpath(os.path.dirname(os.path.realpath(__file__)))
+OpenCVCamera.set_video_source(os.path.join(ROOT_DIR, '1.mp4'))
 
 app = Flask(__name__)
 
@@ -33,6 +38,13 @@ def gen(camera):
 def video_feed():
     """Video streaming route. Put this in the src attribute of an img tag."""
     return Response(gen(Camera()),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
+
+
+@app.route('/video_feed_opencv')
+def video_feed_opencv():
+    """Video streaming route for opencv."""
+    return Response(gen(OpenCVCamera()),
                     mimetype='multipart/x-mixed-replace; boundary=frame')
 
 
--- camera_opencv.py
+++ camera_opencv.py
@@ -15,9 +15,12 @@ class Camera(BaseCamera):
         if not camera.isOpened():
             raise RuntimeError('Could not start camera.')
 
-        while True:
-            # read current frame
-            _, img = camera.read()
+        try:
+            while True:
+                # read current frame
+                _, img = camera.read()
 
-            # encode as a jpeg image and return it
-            yield cv2.imencode('.jpg', img)[1].tobytes()
+                # encode as a jpeg image and return it
+                yield cv2.imencode('.jpg', img)[1].tobytes()
+        except:
+            print ("Video ended.")
